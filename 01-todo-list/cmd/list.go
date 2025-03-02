8f7141d51b56e31e684aac25c29bcbe07d5723ae
JuanParraIV <jmparra1993@gmail.com>
2025-03-02 14:15:55 -0500

    feat: creating todo list cli application

---
+++ 01-todo-list/cmd/list.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+	"todo-list/todo"
+
+	"github.com/spf13/cobra"
+)
+
+func listRun(cmd *cobra.Command, args []string) {
+	items, err := todo.ReadItems(dataFile)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	w := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
+	for _, i := range items {
+		fmt.Fprintln(w, i.PrettyP()+"\t"+i.Text+"\t")
+	}
+	w.Flush()
+}
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list the todos",
+	Long:  `listing the todos`,
+	Run:   listRun,
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ 01-todo-list/todo/todo.go
@@ -0,0 +1,61 @@
+package todo
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Item struct {
+	Text     string
+	Priority int
+}
+
+func SaveItems(filename string, items []Item) error {
+	b, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filename, b, 0644)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	return nil
+}
+
+func ReadItems(filename string) ([]Item, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return []Item{}, err
+	}
+
+	var items []Item
+
+	if err := json.Unmarshal(b, &items); err != nil {
+		return []Item{}, nil
+	}
+	return items, nil
+}
+
+func (i *Item) SetPriority(pri int) {
+	switch pri {
+	case 1:
+		i.Priority = 1
+	case 3:
+		i.Priority = 2
+	default:
+		i.Priority = 2
+	}
+}
+
+func (i *Item) PrettyP() string {
+	if i.Priority == 1 {
+		return "(1)"
+	}
+	if i.Priority == 3 {
+		return "(3)"
+	}
+	return " "
+}
